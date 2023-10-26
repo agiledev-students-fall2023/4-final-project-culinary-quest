b4e4751de02a70ed706da848b2aa99ef9481c27d
Anna Lovellette <al6180@nyu.edu>
2023-10-26 13:52:58 -0400

    add prof pic placeholder to change password & settings pages

--- front-end/src/SettingsPage.jsx
+++ front-end/src/SettingsPage.jsx
@@ -6,13 +6,16 @@ function SettingsPage() {
     const navigate = useNavigate();
 
     return (
-        <div className="settings-container">
-            <input type="text" placeholder="Change username" />
-            <button className="input-like-button" onClick={() => navigate('/change-password')}>Change Password</button>
-            <input type="text" placeholder="Update contact information" />
-            <div className="button-group">
-                <button onClick={() => navigate('/')}>Logout</button>
-                <button className="danger">Delete Account</button>
+        <div className="profile-container">
+            <div className="profile-picture"></div>
+            <div className="settings-container">
+                <input type="text" placeholder="Change Username" />
+                <button className="input-like-button" onClick={() => navigate('/change-password')}>Change Password</button>
+                <input type="text" placeholder="Update Contact Information" />
+                <div className="button-group">
+                    <button onClick={() => navigate('/')}>Logout</button>
+                    <button className="danger">Delete Account</button>
+                </div>
             </div>
         </div>
     );
