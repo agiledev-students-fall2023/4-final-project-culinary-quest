c8da2559548616359e605ca8881a490c511d18e0
Jason-SL-Zhang <sz3042@nyu.edu>
2023-10-25 23:41:09 -0400

    login and create

---
+++ front-end/src/login.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./login.css";
+
+const Login = () => {
+  return (
+    <div className="Login">
+      <div className="div">
+        <div className="forgot-password-sign">
+          <div className="text-wrapper">Forgot Password?</div>
+        </div>
+        <div className="sign-in-button">
+          <div className="overlap-group">
+            <div className="text-wrapper-2">Log In</div>
+          </div>
+        </div>
+        <div className="PASSWORD-sign-in">
+          <div className="overlap">
+            <div className="text-wrapper-3">Password</div>
+          </div>
+        </div>
+        <div className="EMAIL-sign-in">
+          <div className="overlap-2">
+            <div className="rectangle" />
+            <div className="text-wrapper-4">Email</div>
+          </div>
+        </div>
+        <div className="app-logo" />
+        <div className="create-account">
+          <p className="don-t-have-an">
+            <span className="span">Don’t have an account? </span>
+            <Link to="/signup" className="text-wrapper-5">Sign Up</Link> {/* Link to CreateAccount component */}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
