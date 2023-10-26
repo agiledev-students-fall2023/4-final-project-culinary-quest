194aed7224ee84063a4eec04cba78ba7d6552777
jp6024 <119539304+jp6024@users.noreply.github.com>
2023-10-25 23:55:58 -0400

    test, adding ingredient_inventory.jsx

--- front-end/src/ingredient_inventory.jsx
+++ front-end/src/ingredient_inventory.jsx
@@ -1,81 +1,36 @@
 import React from "react";
-import { Hamburger } from "../../components/Hamburger";
-import { LogoButton } from "../../components/LogoButton";
-import "./style.css";
+import "./ingredient_inventory.css";
 
-export const Index = () => {
+export const Ingredient_Inventory = () => {
   return (
     <div className="index">
-      <div className="div">
-        <div className="overlap">
-          <div className="text-wrapper-2">[INGREDIENT NAME]</div>
-        </div>
-        <button className="frame">
-          <button className="submit-button">
-            <div className="overlap-group">
-              <div className="submit-button-2" />
-              <div className="text-wrapper-3">Search</div>
-            </div>
-          </button>
-        </button>
-        <div className="overlap-2">
-          <div className="ingredient-inventory">
-            <div className="ingredients">
-              <div className="rectangle" />
-              <div className="INGREDIENT-NAME">
-                INGREDIENT NAME
-                <br />
-                <br />
-                description here
-              </div>
-            </div>
-            <div className="ingredients-2">
-              <div className="rectangle" />
-              <div className="INGREDIENT-NAME">
-                INGREDIENT NAME
-                <br />
-                <br />
-                description here
-              </div>
-              <div className="ingredients-3">
-                <div className="rectangle" />
-                <div className="INGREDIENT-NAME">
-                  INGREDIENT NAME
-                  <br />
-                  <br />
-                  description here
-                </div>
-              </div>
-            </div>
-            <div className="ingredients-4">
-              <div className="rectangle" />
-              <div className="INGREDIENT-NAME">
-                INGREDIENT NAME
-                <br />
-                <br />
-                description here
-              </div>
-              <div className="ingredients-3">
-                <div className="rectangle" />
-                <div className="INGREDIENT-NAME">
-                  INGREDIENT NAME
-                  <br />
-                  <br />
-                  description here
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="INGREDIENT-NAME-2">
-            INGREDIENT NAME
-            <br />
-            <br />
-            description here
+        <h1>YOUR INGREDIENTS</h1>
+      <div className="columns">
+        <div className="column">
+          <img src="image_url" alt="Ingredient Image" />
+          <div className="ingredient-description">
+            <h3>Ingredient Name</h3>
+            <p>Description here</p>
           </div>
         </div>
-        <Hamburger className="hamburger-instance" />
-        <LogoButton className="logo-button-instance" />
+        <div className="column">
+          <img src="image_url" alt="Ingredient Image" />
+          <div className="ingredient-description">
+            <h3>Ingredient Name</h3>
+            <p>Description here</p>
+          </div>
+        </div>
+        <div className="column">
+          <img src="image_url" alt="Ingredient Image" />
+          <div className="ingredient-description">
+            <h3>Ingredient Name</h3>
+            <p>Description here</p>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
+
+
+export default Ingredient_Inventory;
