13f9bfb5576ae83d44df36acd1b297d811616ebd
Anna Lovellette <al6180@nyu.edu>
2023-10-26 10:55:53 -0400

    add routes & fix header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
-// App.js
 import React from 'react';
-import Header from './header';  // Import the header component
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './header';
 import SettingsPage from './SettingsPage';
 import IndvRecipe from './indv_recipe';
 import CreateAccount from './createAccount';
@@ -10,18 +11,39 @@ import Ingredient_Inventory from './ingredient_inventory';
 import Ingredient_Search from './ingredient_search';
 import ChangePassword from './ChangePassword';
 
+// function App() {
+//   return (
+//     <div className="App"> 
+//       <Header />
+//       <Login />
+//       <CreateAccount />
+//       <ForgotPassword />
+//       <IndvRecipe />
+//       <Ingredient_Inventory />
+//       <Ingredient_Search />
+//       <SettingsPage />
+//       <ChangePassword />
+//     </div>
+//   );
+// }
+
 function App() {
   return (
-    <div className="App"> 
-      <Header />
-      <Login />
-      <CreateAccount />
-      <ForgotPassword />
-      <IndvRecipe />
-      <Ingredient_Inventory />
-      <Ingredient_Search />
-      <SettingsPage />
-      <ChangePassword />
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/create-account" element={<CreateAccount />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/recipe" element={<IndvRecipe />} />
+          <Route path="/inventory" element={<Ingredient_Inventory />} />
+          <Route path="/search" element={<Ingredient_Search />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/change-password" element={<ChangePassword />} />
+          <Route path="/" element={<Login />} exact />  {/* This is the default route */}
+        </Routes>
+      </Router>
     </div>
   );
 }
