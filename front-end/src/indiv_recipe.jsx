71dd50826196e7a8a8b148870606713d2accae00
Jeremy Cao <typeset268@gmail.com>
2023-10-27 16:37:17 -0400

    Additional comments; started recipe list section

--- front-end/src/indiv_recipe.jsx
+++ front-end/src/indiv_recipe.jsx
@@ -6,6 +6,8 @@ const IndivRecipe = props => {
     return (
         <div className = "INDIV-RECIPE">
             {/*<Header />*/}
+
+            {/* Displays the recipe name and photo ontop of each other */}
             <div className = "name">
                 <h1>[Recipe Name]</h1>
             </div>
@@ -14,11 +16,14 @@ const IndivRecipe = props => {
                 <p>RECIPE PHOTO</p>
             </div>
 
+            {/* Main recipe text is kept in a separate div one ontop of the other */}
             <div className = "recipe-text">
+                {/* Separate class to allow for italics and different font color/size */}
                 <div className = "description">
                     <p>[Description]</p>
                 </div>
 
+                {/* Main ingredient list and steps can be displayed using standard text/article formating */}
                 <div className = "main-text">
                     <h2>Ingredients</h2>
                     <p>[Ingredients]</p>
@@ -28,7 +33,8 @@ const IndivRecipe = props => {
                     <p>[Steps]</p>
                 </div>
             </div>
-
+            
+            {/* Button to return to recipe inventory -- CURRENTLY A NON-FUNCTIONAL PLACEHOLDER */}
             <div className = "button">
                 <p>Return to Recipes</p>
             </div>
--- front-end/src/recipe_inventory.jsx
+++ front-end/src/recipe_inventory.jsx
@@ -6,8 +6,13 @@ const recipeInv = props => {
     return (
         <div className = "RECIPE-INV">
             {/*<Header />*/}
-            <div className = "search-bar"> {/* Should be a column flexbox */}
-                <input type="text" placeholder="Search for a recipe" />
+
+            {/* --- SEARCH BAR & FILTERS--- */}
+            <div className = "search"> {/* Should be a column flexbox */}
+                <div className = "search-bar">
+                    <input type="text" placeholder="Search for a recipe" />
+                </div>
+
                 <div className = "search-tools"> {/* Should be a row flexbox (row of elements) */}
                     <div className = "square" /> {/* The filter toggle button */}
                     <p>Filter by available ingredients</p> {/* The text next to the filter toggle button */}
@@ -15,6 +20,11 @@ const recipeInv = props => {
                         <p>Search</p>
                     </div>
                 </div>
+            </div>
+
+            {/* --- RECIPE LIST --- */}
+            <div className = "recipe-list"> {/* Should be a column flexbox */}
+                
             </div>
         </div>
     );
