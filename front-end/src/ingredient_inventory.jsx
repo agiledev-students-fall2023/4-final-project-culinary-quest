f3c4a9fd6322825412b16aacc9a761a19655eb57
jp6024 <119539304+jp6024@users.noreply.github.com>
2023-10-29 22:54:39 -0400

    updating ingredient search and inventory

--- front-end/src/ingredient_inventory.jsx
+++ front-end/src/ingredient_inventory.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 import "./ingredient_inventory.css";
 
-export const Ingredient_Inventory = () => {
+const Ingredient_Inventory = () => {
   return (
-    <div className="index">
-        <h1>YOUR INGREDIENTS</h1>
+    <div className="container">
+      <h1>Your Ingredients</h1>
+      <div className="spacer"></div>
       <div className="columns">
         <div className="column">
-          <img src="image_url" alt="Ingredient Image" />
+          <img src="https://shorturl.at/gHJ89" alt="Ingredient Image" />
           <div className="ingredient-description">
             <h3>Ingredient Name</h3>
             <p>Description here</p>
           </div>
         </div>
         <div className="column">
-          <img src="image_url" alt="Ingredient Image" />
+          <img src="https://shorturl.at/gHJ89" alt="Ingredient Image" />
           <div className="ingredient-description">
             <h3>Ingredient Name</h3>
             <p>Description here</p>
           </div>
         </div>
         <div className="column">
-          <img src="image_url" alt="Ingredient Image" />
+          <img src="https://shorturl.at/gHJ89" alt="Ingredient Image" />
+          <div className="ingredient-description">
+            <h3>Ingredient Name</h3>
+            <p>Description here</p>
+          </div>
+        </div>
+        <div className="column">
+          <img src="https://shorturl.at/gHJ89" alt="Ingredient Image" />
           <div className="ingredient-description">
             <h3>Ingredient Name</h3>
             <p>Description here</p>
@@ -31,6 +39,5 @@ export const Ingredient_Inventory = () => {
     </div>
   );
 };
-
 
 export default Ingredient_Inventory;
