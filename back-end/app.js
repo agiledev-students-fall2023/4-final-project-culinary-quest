f79e759a06c60e7727bc87a27f48fed708d0ef07
zc2126 <zc2126@nyu.edu>
2023-11-05 15:22:31 -0500

    server starts now?

--- back-end/app.js
+++ back-end/app.js
@@ -4,5 +4,9 @@ const app = express() // instantiate an Express object
 
 // we will put some server logic here later...
 
+app.get("/home", (req, res) => {
+    res.send("please send help [crying_face]")
+  })
+
 // export the express app we created to make it available to other modules
 module.exports = app
