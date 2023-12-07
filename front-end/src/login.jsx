97249abfad4a05965e9ab2a3e4ac786450c21c50
zc2126 <zc2126@nyu.edu>
2023-12-07 13:12:16 -0500

    update

--- front-end/src/login.jsx
+++ front-end/src/login.jsx
@@ -11,7 +11,7 @@ function Login() {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post("https://whale-app-bio98.ondigitalocean.app/api/login", {
+      const response = await axios.post("./api/login", {
         username,
         password,
       });
